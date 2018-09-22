08016a07bef1717e5bcaa006e1ad18e235178610
ashish rana <livelifelively@ashishs-MacBook-Pro.local>
2018-09-22 17:54:06 +0530

    spacing changes

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -4,15 +4,18 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 const commonConfig = require('./common.config');
 
-module.exports = merge(commonConfig, {
-  mode: 'production',
-  entry: './index.tsx',
-  output: {
-    filename: 'js/bundle.[hash].min.js',
-    path: resolve(__dirname, '../../dist'),
-    publicPath: '/',
-  },
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ],
-});
+module.exports = merge(
+  commonConfig,
+  {
+    mode: 'production',
+    entry: './index.tsx',
+    output: {
+      filename: 'js/bundle.[hash].min.js',
+      path: resolve(__dirname, '../../dist'),
+      publicPath: '/',
+    },
+    plugins: [
+      new BundleAnalyzerPlugin()
+    ]
+  }
+);
